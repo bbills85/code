9058215ff9503720531979c8298ff45ba1c58acc
bbills85 <wsinkey@gmail.com>
2015-04-14 21:45:03 +0200

    updates

--- Python InfoSec SPSE/Module-4/tryme.py
+++ Python InfoSec SPSE/Module-4/tryme.py
@@ -17,8 +17,9 @@ import urllib
 import mechanize
 import threading
 from bs4 import BeautifulSoup
+import MySQLdb
 import timeit
-
+import time
 #threadLock = threading.Lock()
 
 class webSpider(threading.Thread):
@@ -33,36 +34,78 @@ class webSpider(threading.Thread):
 		self.thread = i
 
 	def run(self):
-		while True:
-			url, depth = self.linkQueue.get()
-			if depth <= self.linkDepth:
-				print "THREADAAAAAAAAAAAAAAAAA: " + str(self.thread)
-				print "LINK DEPTH: " + str(self.linkDepth)
-				print "CURRENT DEPTH: " + str(depth)
-				print "CURRENT URL: "+ url
-				print "AT DEPTH: " + str(depth)
-				# erorr check login to ftp site
-#				try:
-				print "[DEBUG] **run()** - Now visiting URL: " + url
-				current_link_list = self.findLinks(url)
-				print "CUURENT LINK LIST:"
-				print current_link_list
-				self.checkList(current_link_list, depth + 1)
-			else:
-				self.linkQueue.task_done()
-				continue
-			
-				print "MASTER LIST"
-				print self.masterList
-	
-			self.linkQueue.task_done()
-		print "\n\nCLEANING UP!"
+#		while True:
 		while not self.linkQueue.empty():
 			url, depth = self.linkQueue.get()
-			print url
-			print depth
-			self.linkQueue.task_done()
+			try:
+				if depth <= self.linkDepth:
+					print "THREADAAAAAAAAAAAAAAAAA: " + str(self.thread)
+					print "LINK DEPTH: " + str(self.linkDepth)
+					print "CURRENT DEPTH: " + str(depth)
+					print "CURRENT URL: "+ url
+					print "AT DEPTH: " + str(depth)
+					print "[DEBUG] **run()** - Now visiting URL: " + url
+#					current_form_list = self.findForms(url)
+					current_link_list = self.findLinks(url)
+					print "CUURENT LINK LIST:"
+					print current_link_list
+					self.checkList(current_link_list, depth + 1)
+				else:
+					self.linkQueue.task_done()
+					continue
+			
+			except:
+		                self.LinkQueue.task_done()
+				continue
+			try:
+ 	              		# connect to db and insert site
+				self.addSQL(self.masterList)
+            		except:
+                		print "can not insert url: {0} into database".format(url)
+            		finally:
+				print "I AM IN FINALLY"
+				self.linkQueue.task_done()
+#				print "\n\nCLEANING UP!"
+#				print threading.current_thread()
+#				while not self.linkQueue.empty():
+#					url, depth = self.linkQueue.get()
+#					print url
+#					print depth
+#					self.linkQueue.task_done()
+#				if self.linkQueue.empty():
+#					"THE PROGRAM WILL END!!!!!!"
+#					return 
 	
+	def addSQL(self, masterList):
+		# Open database connection
+		db = MySQLdb.connect("localhost","wsinkey","","testdb" )
+
+		# prepare a cursor object using cursor() method
+		cursor = db.cursor()
+
+		# Drop table if it already exist using execute() method.
+		cursor.execute("DROP TABLE IF EXISTS LINKS")
+
+		# Create table as per requirement
+		sql = """CREATE TABLE LINKS (Id INT PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(50))"""
+		cursor.execute(sql)
+		db.commit()
+
+		for link in masterList:
+			sql = """INSERT INTO LINKS (Name) VALUES (%s)"""
+			arg = link
+			cursor.execute(sql, arg)
+			db.commit()
+
+
+   		cursor.execute("SELECT * FROM LINKS")
+		rows = cursor.fetchall()
+
+		for row in rows:
+			print row
+
+		# disconnect from server
+		db.close()
 
 	def checkList(self, links, current_depth):
 		for link in links:
@@ -74,6 +117,21 @@ class webSpider(threading.Thread):
 				self.masterList.append(link)
 				self.linkQueue.put((link, current_depth))
 #				threadLock.release()
+
+	def findForms(self, url):
+		forms = []
+
+	        if self.check_url(url) == 200:
+			html = urllib.urlopen(url).read()
+			soup = BeautifulSoup(html)
+
+			forms = soup.find_all('form')
+			for form in forms:
+#				print form
+				inputs = form.find_all('input')
+				for input in inputs:
+					print form.name
+					print "  -> %s" % (input.attrs) 
 
 	def findLinks(self, url):
         	links = []
@@ -101,6 +159,7 @@ class webSpider(threading.Thread):
 					print "[DEBUG] **findLinks()** - Discovered URL: " + mechanize.urljoin(url, link.url)
 					links.append(mechanize.urljoin(url, link.url))
 #					threadLock.release()
+					
 			
 			return links
                 else:
@@ -169,6 +228,8 @@ def main():
 
 	linkQueue.put((url, 0))
 
+	threads = []
+
 	for i in range(5):
 		print "[+] Creating Thread: %d ..." % i
 #		# calls the class ftpLogin sending the Queue and number range
@@ -179,12 +240,12 @@ def main():
 		# start thread
 		workerThread.start()
 		print "[+] Thread %d created ..." % i
+		threads.append(workerThread)
 
 	# waits until all items in the Queue have been
 	# processed (related to *.taskdone())
-	
 	linkQueue.join()
-
+#	time.sleep(1)
 	print "DONE"
 	print masterList
 	stop = timeit.default_timer()
