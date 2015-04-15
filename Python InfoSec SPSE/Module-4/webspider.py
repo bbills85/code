eb3eb850eac60413de250047a22f8f5712a3318e
bbills85 <wsinkey@gmail.com>
2015-04-15 21:36:17 +0200

    Updates

--- Python InfoSec SPSE/Module-4/webspider.py
+++ Python InfoSec SPSE/Module-4/webspider.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
 
+########################################################################################################
+# Webspider Program
+# Date: 15 April 2015
+# Author: bbills85
+# Version: 1
+# Tested with: Python 2.7.6 on Ubuntu 14.04.2 LTS
+########################################################################################################
+# References:
 # http://www.tutorialspoint.com/python/python_command_line_arguments.htm
 # http://pymotw.com/2/getopt/
 # http://stackoverflow.com/questions/15993238/python-invalid-literal-for-int-base-10
@@ -8,126 +16,214 @@
 # http://stackoverflow.com/questions/15956597/beautifulsoup-parsing-and-writing-the-data-in-a-text-file
 # http://www.tutorialspoint.com/python/python_files_io.htm
 # http://www.tutorialspoint.com/python/python_database_access.htm
+# https://docs.python.org/2/library/argparse.html
+# http://pymotw.com/2/argparse/
+########################################################################################################
 
-import getopt
-import sys
+import argparse
 import Queue
-import sqlite3
 import urllib
 import mechanize
+import threading
 from bs4 import BeautifulSoup
+import MySQLdb
+import timeit
 
-linkQueue = Queue.Queue()
-url_list = []
-depth = 0
+threadLock = threading.Lock()
 
-def findLinks(url):
-	links = []
-	print "current url: " + url + '\n'
-	if check_url(url):
-		br = mechanize.Browser()
-		br.open(url)
+class webSpider(threading.Thread):
+	"""class that instantiates a thread"""
+	def __init__(self, linkQueue, linkDepth, masterList, thread):
+		# call the parent class init???????????????
+		threading.Thread.__init__(self)
+		# store the passed in variables into the class variables
+		self.linkQueue = linkQueue
+		self.linkDepth = linkDepth
+		self.masterList = masterList
+		self.thread = thread
 
-		for link in br.links():
-			if ".html" in link.url:
-				print "Discovered url: " + mechanize.urljoin(url, link.url)
-				links.append(mechanize.urljoin(url, link.url))
-	else:
-		print "BAD LINK"
-		sys.exit(0)
-	return links
+	def run(self):
+		while not self.linkQueue.empty():
+			url, depth = self.linkQueue.get()
+			try:
+				if depth <= self.linkDepth:
+					print "[DEBUG] **run()** - Thread: %d" % int(self.thread + 1)
+					print "[DEBUG] **run()** - Current URL: "+ url
+					print "[DEBUG] **run()** - Current Depth: " + str(depth)
 
-def checkList(links, current_depth):
-	global url_list
-	global linkQueue
-	print current_depth
-	for link in links:
-		if not link in url_list:
-			print "ADD LINK: " + link
-			url_list.append(link)
-			linkQueue.put((link, current_depth))
+					current_link_list = self.findLinks(url)
 
-def webspider(url, depth):
-	global url_list
-	global linkQueue
+					print "[DEBUG] **run()** - Current Link List:"
+					print current_link_list
+
+					self.checkList(current_link_list, depth + 1)
+				else:
+					self.linkQueue.task_done()
+					continue
+			
+			except:
+		                self.linkQueue.task_done()
+				continue
+			try:
+ 	              		# connect to db and insert site
+				#current_form_list = self.findForms(self.masterList)
+				(formName, formInput, formAttrs) = self.findForms(self.masterList)
+				print formName
+				print formInput
+				print formAttrs
+
+				print "[DEBUG] **run()** - Current Form List:"
+				print current_form_list
+            		except:
+                		print "can not insert url: {0} into database".format(url)
+            		finally:
+				print "[DEBUG] **run()** - Generating SQL DB"
+				self.addSQL(self.masterList)
+				self.linkQueue.task_done()
+#					return 
 	
-	current_depth = 0;
+	def addSQL(self, masterList):
+		# Open database connection
+		db = MySQLdb.connect("localhost","wsinkey","","testdb" )
 
-	linkQueue.put((url, current_depth))
+		# prepare a cursor object using cursor() method
+		cursor = db.cursor()
 
-	while current_depth <= depth:
-		print current_depth
+		# Drop table if it already exist using execute() method.
+		cursor.execute("DROP TABLE IF EXISTS LINKS")
 
-		url, b = linkQueue.get()
-		print "grabbing: " + url + " : Depth: " + str(b)
+		# Create table as per requirement
+		sql = """CREATE TABLE LINKS (Id INT PRIMARY KEY AUTO_INCREMENT, Name VARCHAR(1000))"""
+		cursor.execute(sql)
+		db.commit()
 
-		links = findLinks(url)
-		checkList(links, current_depth + 1)
-		print url_list
-		while not linkQueue.empty() and current_depth <= depth:
-			url, b = linkQueue.get()
-			print b
-			if b == current_depth:
-				print "Visiting: " + url
-				links = findLinks(url)
-				checkList(links, current_depth + 1)
+		for link in masterList:
+			sql = """INSERT INTO LINKS (Name) VALUES (%s)"""
+			arg = link
+			cursor.execute(sql, arg)
+			db.commit()
+
+
+   		cursor.execute("SELECT * FROM LINKS")
+		rows = cursor.fetchall()
+
+		for row in rows:
+			print row
+
+		# disconnect from server
+		db.close()
+
+	def checkList(self, links, current_depth):
+		for link in links:
+			if link in self.masterList:
+				print "[DEBUG] **checkList** - URL already in list: " + link
 			else:
-				linkQueue.put((url, b))
-				current_depth += 1
+				print "[DEBUG] **checkList** - Add URL: " + link + " at depth: " + str(current_depth)
+#				threadLock.acquire()
+				self.masterList.append(link)
+				self.linkQueue.put((link, current_depth))
+#				threadLock.release()
 
-#		current_depth += 1
+	def findForms(self, masterList):
+		forms = []
 
-	print url_list
+		for link in masterList:
+			print "[DEBUG] **findForms** - Current URL to Search for Forms: " + str(link)
+
+			html = urllib.urlopen(link).read()
+			soup = BeautifulSoup(html)
+
+			forms = soup.find_all('form')
+
+			for form in forms:
+				inputs = form.find_all('input')
+				for input in inputs:
+					formName = form.name
+					formInput = input
+					formAttrs = input.attrs
+		
+		return (formName, formInput, formAttrs)
+
+	def findLinks(self, url):
+        	links = []
+	        print "[DEBUG] **findLinks()** - Current URL: " + url
+
+	        if self.check_url(url) == 200:
+			br = mechanize.Browser()
+			br.open(url)
+
+			for link in br.links():
+				if 'html' in link.url:
+#					threadLock.acquire()
+					print "[DEBUG] **findLinks()** - Discovered URL: " + mechanize.urljoin(url, link.url)
+					links.append(mechanize.urljoin(url, link.url))
+#					threadLock.release()
+					
+			
+			return links
+                else:
+                        print "[DEBUG] **findLinks()** HTTP Code Error URL not recorded"
+
+                        return links
 
 
-def check_url(url):
-	httpResponse = urllib.urlopen(url)
+	def check_url(self, url):
+		try:
+			br = mechanize.Browser()
+                        br.open(url)
 
-	if httpResponse.code == 200:
-		print httpResponse.code
-		return True
-	else:
-		return False
-
-def usage():
-        print "Webspider"
-        print
-        print "Usage: " + sys.argv[0] + " -u http://www.test.com -d 3"
-        print "-u --url		- listen on [host]:[port] for incoming connections"
-        print "-d --depth =		- execute the given file upon receiving a connection"
-        print
-        print
-        print "Examples: "
-        sys.exit(0)
+			return 200
+		except:
+			return 0
 
 def main():
-	global depth
-	global linkQueue
+	start = timeit.default_timer()
 
-	# execute usage if no arguments provided
-        if not len(sys.argv[1:]):
-                usage()
+	linkQueue = Queue.Queue()
+	masterList = []
 
-        # read the commandline options
-        try:
-                # parses all command line arguments into lists starting at position 1 and store in opts
-		# options that require an argument should be followed by a colon (:)
-                opts, args = getopt.getopt(sys.argv[1:], "hu:d:", ["help", "url", "depth"])
-        except getopt.GetoptError as err:
-                print str(err)
-                usage()
+	parser = argparse.ArgumentParser(description =
+					 'Webspider for links and form data')
+	parser.add_argument('-u', '--url', required = True, dest = 'url',
+			    action = 'store', metavar = 'http://www.google.com',
+			    help = 'base url to web spider')
+	parser.add_argument('-d', '--depth', required = False, dest = 'depth',
+			    action = 'store', metavar = '3', default = 0, type = int,
+			    help = 'depth for web spider')
+	parser.add_argument('-t', '--threads', required = False, dest = 'threads',
+			    action = 'store', metavar = '5', default = 1, type = int,
+			    help = 'number of threads')
+	parser.add_argument('--version', action = 'version', version = '%(prog)s 1.0')
 
-        for o, a in opts:
-                if o in ("-h", "--help"):
-                        usage()
-                elif o in ("-u", "--url"):
-                        url = a
-                elif o in ("-d", "--depth"):
-                        depth = int(a)
-                else:
-			assert False, "Unhandled Option"
-	print "depth is: " + str(depth)
-	webspider(url, depth)
+	args = parser.parse_args()
+
+	print "[DEBUG] **main()** - Starting Webspider at Depth: " + str(args.depth)
+
+	linkQueue.put((args.url, 0))
+
+	masterList.append(args.url)
+	
+	for thread in range(args.threads):
+		print "[DEBUG] **main()** - Creating Thread: %d" % int(thread + 1)
+		# calls the class ftpLogin sending the Queue and number range
+		workerThread = webSpider(linkQueue, args.depth, masterList, thread)
+		# sets Daemon to True; when main thread terminates
+		# all threads terminate
+		workerThread.setDaemon(True)
+		# start thread
+		workerThread.start()
+		print "[DEBUG] **main()** - Thread %d created" % int(thread + 1)
+
+	# waits until all items in the Queue have been
+	# processed (related to *.taskdone())
+	linkQueue.join()
+
+	print "[DEBUG] **main()** - Master List"
+	print masterList
+
+	stop = timeit.default_timer()
+	print "[DEBUG] **main()** Run Time: %.2f seconds" % float(stop - start)
+	print "[DEBUG] **main()** - Program Terminated"
 
 if __name__ == "__main__":
 	main()
